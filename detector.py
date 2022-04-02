3405ca8ae8311d15e61d76c896ba3a9e167957b2
Luis Vinicius <camperqueodeiacamper@gmail.com>
2022-04-01 23:13:14 -0300

    descanso automatico

---
+++ detector.py
@@ -0,0 +1,55 @@
+import cv2
+import mediapipe as mp
+import screeninfo
+
+
+webcam = cv2.VideoCapture(0)
+
+solucao_reconhecimento_rosto = mp.solutions.face_detection
+
+reconhecedor_rostos = solucao_reconhecimento_rosto.FaceDetection()
+
+desenho = mp.solutions.drawing_utils
+
+contador = 0
+
+while True:
+
+    verificador, frame = webcam.read()
+    if not verificador:
+        break
+
+    listas_rostos = reconhecedor_rostos.process(frame)
+
+    if listas_rostos.detections:
+        if contador >= 3:
+            contador = 0
+            cv2.destroyWindow("imagebackground")
+
+        #os.system("taskkill /im imagebackground")
+        print('rosto encontrado!')
+        for rosto in listas_rostos.detections:
+            desenho.draw_detection(frame, rosto)
+    else:
+        contador += 1
+
+    if contador == 3:
+
+        screen = screeninfo.get_monitors()[0]
+        width, height = screen.width, screen.height
+
+        print('rosto não encontrado')
+        imagem = cv2.imread("background.jpg")
+        cv2.namedWindow("imagebackground", cv2.WND_PROP_FULLSCREEN)
+        cv2.moveWindow("imagebackground", screen.x - 1, screen.y - 1)
+        cv2.setWindowProperty("imagebackground", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.moveWindow("imagebackground", 0, 0)
+        cv2.imshow("imagebackground", imagem)
+
+    
+
+    key=cv2.waitKey(1000)
+    if key == 27:
+        break
+
+webcam.release()
